e5688d2942763da8eee2ca29bea471590cf9014e
santiago <santiagovillahermosa@gmail.com>
2023-06-20 13:57:29 -0400

    test: fixing url 6

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/map/"
+  base: "https://santiago1397.github.io/map/"
 })
