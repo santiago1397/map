8c47698c531b3d40a890415a31cf017c6bf9c16e
santiago <santiagovillahermosa@gmail.com>
2023-06-20 16:55:50 -0400

    soadmaoskd

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
       ),
     },
  
-  ], {basename: "/map"});
+  ], {basename: "https://santiago1397.github.io/map/"});
 
 createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
